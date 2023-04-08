1132670b71b40f942e5540509a4f1f279d7e5ed7
Henok Belachew <henokbelachewstu@gmail.com>
2023-04-08 13:58:52 +0300

    must work

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to nginx on LXD container web1!</title>
-<style>
-  body {
-      width: 35em;
-      margin: 0 auto;
-      font-family: Tahoma, Verdana, Arial, sans-serif;
+# creating a custom HTTP header response with Puppet.
+
+class nginx {
+  package { 'nginx':
+    ensure => installed,
   }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx on LXD container web1!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+
+  file { '/etc/nginx/conf.d/custom.conf':
+    ensure  => file,
+    content => "add_header X-Served-By $::hostname;",
+    notify  => Service['nginx'],
+  }
+}
